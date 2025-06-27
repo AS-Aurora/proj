348ec179302da31fa2fe6375279870c2818368f5
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-06-27 20:59:24 +0530

    Added the login backend logic

--- base/views.py
+++ base/views.py
@@ -3,18 +3,40 @@ from django.db.models import Q
 from .models import Room, Topic
 from uuid import UUID
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout 
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            messages.error(request, 'Username or password is incorrect')
+            return redirect('login')  # Redirect to login page if authentication fails
+        else:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'base/login.html')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |  # Filter rooms by topic name
         Q(description__icontains=q) |  # Filter rooms by description
         Q(name__icontains=q) | # Filter rooms by name
         Q(host__username__icontains=q)  # Filter rooms by host username
         )  # Fetch all rooms from the database
+    
     room_count = rooms.count()  # Count the number of rooms
     topics = Topic.objects.all()  # Fetch all topics from the database
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'showSearchBar': True}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
