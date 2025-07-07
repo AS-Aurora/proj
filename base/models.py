9ca9812ceda8d12c71d4c91863a06374989094da
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-07-07 08:30:24 +0530

    Some minor changes

--- base/models.py
+++ base/models.py
@@ -9,13 +9,11 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    # id = models.CharField(max_length=10, primary_key=True)  # Using CharField for a custom ID
-    # If you want to use UUID as primary key, uncomment the following line and comment the line above
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)  # Foreign key to Topic, can be null
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # Foreign key to User, can be null
+    host = models.ForeignKey(User, on_delete=models.CASCADE)  # Foreign key to User, can be null
     participants = models.ManyToManyField(User, related_name='participants', blank=True)  # Many-to-many relationship with User
     updated = models.DateTimeField(auto_now=True) #this will update the timestamp whenever the model is saved
     created = models.DateTimeField(auto_now_add=True) #this will store the timestamp when it was first created
